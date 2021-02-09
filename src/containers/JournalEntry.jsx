f6819ceaecb2566d1a70924ff40813f079350446
GBelonozhko <44050866+GBelonozhko@users.noreply.github.com>
2021-02-09 09:53:51 -0800

    commit

--- src/containers/JournalEntry.jsx
+++ src/containers/JournalEntry.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
-import Container from "@material-ui/core/Container";
+import { Container, Card, TextField, Grid,List, ListItem } from "@material-ui/core";
+import Rating from '@material-ui/lab/Rating';
+
 const Journal = () => {
+
+  const [value, setValue] = React.useState(2);
+
   return (
     <div>
       <Container>
-        <TextareaAutosize
-          aria-label='minimum height'
-          rowsMin={10}
-          placeholder='Minimum 3 rows'
-        
-   
-        />
+        <Grid
+          container
+          direction='row'
+          justify='space-evenly'
+          alignItems='center'>
+          <Grid item xs={8}>
+        <Card>
+          <TextField fullWidth multiline={true} margin='normal'/>
+        </Card>
+        </Grid>
+
+        <Grid item xs={3}>
+        <Card>
+        <List>
+        <ListItem>
+        health
+        <Rating name="pristine" value={null} />
+        </ListItem>
+        <ListItem>
+        DEV
+        <Rating name="pristine" value={null} />
+        </ListItem>
+        <ListItem>
+        Prof
+        <Rating name="pristine" value={null} />
+        </ListItem>
+        </List>
+        </Card>
+        </Grid>
+
+        </Grid>
       </Container>
     </div>
   );
