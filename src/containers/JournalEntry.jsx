dac61402a922cb6d33b8842fdb61981177d0a3f4
swis oi <odessagreg@gmail.com>
2021-02-15 17:22:24 -0800

    m

--- src/containers/JournalEntry.jsx
+++ src/containers/JournalEntry.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+
 import {
   Container,
   Card,
@@ -10,7 +10,9 @@ import {
   ListItemText,
   Button,
   Typography,
-  CardHeader,Divider
+  CardHeader,
+  Divider,
+  CircularProgress,
 } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
 import { useSelector, useDispatch } from "react-redux";
@@ -21,6 +23,8 @@ import {
   initTotalTasks,
   setCompleteCount,
 } from "../store/actions/ToDo.Action";
+import { updateObject } from "../shared/utility";
+import RatingComp from "../components/Rating";
 
 const Journal = () => {
   const dispatch = useDispatch();
@@ -30,69 +34,88 @@ const Journal = () => {
   const completeCount = useSelector((state) => state.Todos.totalCompletes);
   const totalTasks = useSelector((state) => state.Todos.totalTasks);
 
-  const [rating, setRating] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [rating, setRating] = useState([
+    { catagory: "Happiness", rating: 0 },
+    { catagory: "Productivity", rating: 0 },
+  ]);
 
   useEffect(() => {
     dispatch(initCompleteCount(userId));
     dispatch(initTodoLists(userId));
     dispatch(initTotalTasks(userId));
+    constructRatings();
   }, []);
 
-const handleChangeRating = (listName, newRating) => {};
+  const constructRatings = () => {
+    let ratingObj = rating;
+    todoLists.map((catagory) => {
+      ratingObj.push({ catagory: catagory, rating: 0 });
+    });
+    setRating(ratingObj);
+  };
 
-const handleSubmitJournal = () =>{};
+  const handleChangeRating = (listName, newRating) => {
+    setIsLoading(true);
+    let ratingObj = rating;
+    const rIndex = ratingObj.findIndex((obj) => obj.catagory === listName);
+    ratingObj[rIndex] = { catagory: listName, rating: newRating };
+    setRating(ratingObj);
+    setIsLoading(false);
+    console.log(ratingObj);
+  };
 
-
+  const handleSubmitJournal = () => {};
 
   return (
     <div>
-      <Container>
-        <Grid
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-        >
-          <Grid item xs={8}>
-          <Typography variant="h3" align="center" color="secondary">
-                  Journal Entry
-                </Typography>
-            <Card>
-              
+      {!isLoading ? (
+        <Container>
+          <Grid
+            container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center"
+          >
+            <Grid item xs={8}>
+              <Typography variant="h3" align="center" color="secondary">
+                Journal Entry
+              </Typography>
+              <Card>
+                <form>
+                  <TextField fullWidth multiline={true} margin="normal" />
+                  <Button type="submit" fullWidth onClick={() => {}}>
+                    Submit
+                  </Button>
+                </form>
+              </Card>
+            </Grid>
 
-                
-              
+            <Grid item xs={3}>
+              <Card>
+                <List>
+                  {rating.map((listName) => (
+                    <ListItem key={listName.catagory} divider>
+                      <ListItemText>{listName.catagory}</ListItemText>
+                      <RatingComp catagory={listName.catagory} value={listName.rating} changeValue={handleChangeRating} />
 
-              <TextField fullWidth multiline={true} margin="normal" />
-              <Button fullWidth>Submit</Button>
-            </Card>
+                      <Rating
+                        name={listName.catagory}
+                        value={listName.rating}
+                        onChange={(event, newValue) => {
+                          handleChangeRating(listName.catagory, newValue);
+                        }}
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              </Card>
+            </Grid>
           </Grid>
-
-          <Grid item xs={3}>
-            <Card>
-              <List>
-                {todoLists.map((listName) => (
-                  <ListItem key={listName} divider>
-                    <ListItemText>{listName}</ListItemText>
-
-                    <Rating name="pristine" value={null} />
-                  </ListItem>
-                ))}
-                <ListItem devider>
-                  <ListItemText>Happiness</ListItemText>
-
-                  <Rating name="pristine" value={null} />
-                </ListItem>
-<Divider/>
-                <ListItem>
-                  <ListItemText>Productivity <br/> {completeCount}</ListItemText>
-                  <Rating name="pristine" value={null} />
-                </ListItem>
-              </List>
-            </Card>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      ) : (
+        <CircularProgress />
+      )}
     </div>
   );
 };
